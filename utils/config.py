6e81d7317754b5d5ccc246a90f56ffabf5bf4cd3
Sinem Özbey <ozbeysi@mef.edu.tr>
2025-12-10 21:31:52 +0300

    aa

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,15 @@
-# utils/config.py
+import os
 
-HOST = "127.0.0.1"
-PORT = 50000  
+# Sunucu (ClientA) hangi arayüzde dinleyecek?
+# Varsayılan: tüm arayüzler (0.0.0.0) ki başka makineden erişilebilsin.
+HOST_BIND = os.getenv("TCP_GAME_BIND_HOST", "0.0.0.0")
+
+# İstemcinin (ClientB) hangi IP'ye bağlanacağı.
+# Varsayılan: localhost. Farklı makineden bağlanacaksanız TCP_GAME_HOST=<sunucu_ip>
+# olarak geçici veya kalıcı şekilde ayarlayın.
+HOST_CONNECT = os.getenv("TCP_GAME_HOST", "127.0.0.1")
+
+PORT = int(os.getenv("TCP_GAME_PORT", "50000"))
 
 GAME_DURATION_SECONDS = 300      
 RESPONSE_TIMEOUT_SECONDS = 5    
@@ -19,4 +27,3 @@ ROLE_B_NAME = "ClientB"
 
 MIN_SEGMENT_SIZE = 1
 MAX_SEGMENT_SIZE = 5  
-
